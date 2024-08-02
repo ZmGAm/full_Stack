f7f11bb13723c2a151df2f0401d3e7ec4438ac8d
zmgamer <85769761+ZmGAm@users.noreply.github.com>
2024-08-02 12:57:08 +0500

    Update backend index.js

--- this_is_for_backend/index.js
+++ this_is_for_backend/index.js
@@ -14,7 +14,7 @@ const app = express()
 app.use(cors(
 
   {
-    origin:["https://frontend-iota-woad.vercel.app/"],
+    origin:["https://frontend-iota-woad.vercel.app"],
     methods:["POST","GET"],
     credential:true
   }
